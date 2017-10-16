a95ea5807d9466016c93dffabd1c97427c35bd3e
Derek <dphtran501@yahoo.com>
2017-10-15 21:55:53 -0700

    finished resetQuiz() and loadNextSuperhero() in QuizActivity.java

--- app/src/main/java/edu/orangecoastcollege/cs273/dtran258/cs273superheroes/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/cs273superheroes/QuizActivity.java
@@ -1,10 +1,20 @@
 package edu.orangecoastcollege.cs273.dtran258.cs273superheroes;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Runs an application that quizzes the user on the "superheroes" of CS 273. An image of a
@@ -23,12 +33,26 @@ import android.widget.TextView;
  */
 public class QuizActivity extends AppCompatActivity
 {
+    private static final String TAG = "CS 273 Superheroes";
+    private static final int SUPERHEROES_IN_QUIZ = 10;
 
+    // Views
     private TextView mQuestionNumberTextView;
-    private ImageView mHeroImageView;
+    private ImageView mSuperheroImageView;
     private TextView mGuessTextView;
     private Button[] mButtons = new Button[4];
     private TextView mAnswerTextView;
+    // Lists
+    private List<Superhero> mAllSuperheroesList;
+    private List<Superhero> mQuizSuperheroesList;
+    // Quiz fields
+    private Superhero mCorrectSuperhero;
+    private int mTotalGuesses;
+    private int mCorrectGuesses;
+
+    private SecureRandom rng;
+    private Handler handler;
+
 
     /**
      * Initializes <code>QuizActivity</code> by inflating its UI.
@@ -43,13 +67,87 @@ public class QuizActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        // Get resources from GUI components
         mQuestionNumberTextView = (TextView) findViewById(R.id.questionNumberTextView);
-        mHeroImageView = (ImageView) findViewById(R.id.heroImageView);
+        mSuperheroImageView = (ImageView) findViewById(R.id.heroImageView);
         mGuessTextView = (TextView) findViewById(R.id.guessTextView);
         mButtons[0] = (Button) findViewById(R.id.button);
         mButtons[1] = (Button) findViewById(R.id.button2);
         mButtons[2] = (Button) findViewById(R.id.button3);
         mButtons[3] = (Button) findViewById(R.id.button4);
         mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        // Load list of all Superheroes from JSON file
+        try
+        {
+            mAllSuperheroesList = JSONLoader.loadJSONFromAsset(this);
+        } catch (IOException e)
+        {
+            Log.e(TAG, "Error loading from JSON", e);
+        }
+
+        mQuizSuperheroesList = new ArrayList<>(SUPERHEROES_IN_QUIZ);
+        rng = new SecureRandom();
+        handler = new Handler();
+
+        // Start quiz
+        resetQuiz();
+    }
+
+    private void resetQuiz()
+    {
+        // Clear quiz fields
+        mTotalGuesses = 0;
+        mCorrectGuesses = 0;
+        mQuizSuperheroesList.clear();
+
+        // Randomly add Superheroes from all Superheroes list to quiz list
+        while (mQuizSuperheroesList.size() < SUPERHEROES_IN_QUIZ)
+        {
+            int randomPosition = rng.nextInt(mAllSuperheroesList.size());
+            if (!mQuizSuperheroesList.contains(mAllSuperheroesList.get(randomPosition)))
+                mQuizSuperheroesList.add(mAllSuperheroesList.get(randomPosition));
+        }
+
+        // Load quiz question
+        loadNextSuperhero();
+    }
+
+    private void loadNextSuperhero()
+    {
+        // Retrieve correct superhero
+        mCorrectSuperhero = mQuizSuperheroesList.remove(0);
+        // Clear answer TextView so it doesn't show text from previous question
+        mAnswerTextView.setText("");
+        // Display current question number
+        int questionNumber = SUPERHEROES_IN_QUIZ - mQuizSuperheroesList.size();
+        mQuestionNumberTextView.setText(getString(R.string.question, questionNumber, SUPERHEROES_IN_QUIZ));
+
+        // Display image of correct superhero
+        try
+        {
+            InputStream stream = getAssets().open(mCorrectSuperhero.getFileName());
+            Drawable image = Drawable.createFromStream(stream, mCorrectSuperhero.getUserName());
+            mSuperheroImageView.setImageDrawable(image);
+        } catch (IOException e)
+        {
+            Log.e(TAG, "Error loading image: " + mCorrectSuperhero.getFileName(), e);
+        }
+
+        // Set each of the four buttons to a random superhero from the all superheroes list
+        do
+        {
+            Collections.shuffle(mAllSuperheroesList);
+        } while(mAllSuperheroesList.subList(0, 4).contains(mCorrectSuperhero));
+
+        for (int i = 0; i < 4; i++)
+        {
+            mButtons[i].setEnabled(true);
+            mButtons[i].setText(mAllSuperheroesList.get(i).getUserName());
+        }
+
+        // Randomly set one of the buttons to the correct superhero
+        mButtons[rng.nextInt(4)].setText(mCorrectSuperhero.getUserName());
+
     }
 }
