c48d23a6ba873a75c1ab3cea50ecf22027efd402
Derek <dphtran501@yahoo.com>
2017-10-15 18:53:57 -0700

    Got references to GUI components in activity_quiz.xml and finished Superhero.java

--- app/src/main/java/edu/orangecoastcollege/cs273/dtran258/cs273superheroes/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/cs273superheroes/QuizActivity.java
@@ -1,15 +1,55 @@
 package edu.orangecoastcollege.cs273.dtran258.cs273superheroes;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+/**
+ * Runs an application that quizzes the user on the "superheroes" of CS 273. An image of a
+ * "superhero" is displayed to the screen, as well as four buttons each with a information that
+ * might be related to the "superhero" shown. The user has to click the button with the information
+ * related to the "superhero" shown. In the end, the user will be shown a percentage indicating how
+ * well they did on the quiz.
+ * <p>
+ *     The user can switch between guessing the name, the super power, or the one thing about the
+ *     super hero by clicking on the settings options menu at the top.
+ * </p>
+ *
+ * @author Derek Tran
+ * @version 1.0
+ * @since October 15, 2017
+ */
 public class QuizActivity extends AppCompatActivity
 {
 
+    private TextView mQuestionNumberTextView;
+    private ImageView mHeroImageView;
+    private TextView mGuessTextView;
+    private Button[] mButtons = new Button[4];
+    private TextView mAnswerTextView;
+
+    /**
+     * Initializes <code>QuizActivity</code> by inflating its UI.
+     *
+     * @param savedInstanceState Bundle containing the data it recently supplied in
+     *                           onSaveInstanceState(Bundle) if activity was reinitialized after
+     *                           being previously shut down. Otherwise it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionNumberTextView = (TextView) findViewById(R.id.questionNumberTextView);
+        mHeroImageView = (ImageView) findViewById(R.id.heroImageView);
+        mGuessTextView = (TextView) findViewById(R.id.guessTextView);
+        mButtons[0] = (Button) findViewById(R.id.button);
+        mButtons[1] = (Button) findViewById(R.id.button2);
+        mButtons[2] = (Button) findViewById(R.id.button3);
+        mButtons[3] = (Button) findViewById(R.id.button4);
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
     }
 }
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtran258/cs273superheroes/Superhero.java
@@ -0,0 +1,94 @@
+package edu.orangecoastcollege.cs273.dtran258.cs273superheroes;
+
+/**
+ * Represents a Superhero for the purposes of the CS 273 Superheroes quiz, including the superhero's
+ * username, name, super power, one thing, and the file name (including path) for his/her
+ * image.
+ *
+ * @author Derek Tran
+ * @version 1.0
+ * @since October 15, 2017
+ */
+
+public class Superhero
+{
+    private String mUserName;
+    private String mName;
+    private String mSuperPower;
+    private String mOneThing;
+    private String mFileName;
+
+    /**
+     * Instantiates a new <code>Superhero</code> given its username, name, superpower, and one
+     * thing about them.
+     * @param userName The username of the <code>Superhero</code>.
+     * @param name The name of the <code>Superhero</code>.
+     * @param superPower The superpower of the <code>Superhero</code>.
+     * @param oneThing The one thing about the <code>Superhero</code>.
+     */
+    public Superhero(String userName, String name, String superPower, String oneThing)
+    {
+        mUserName = userName;
+        mName = name;
+        mSuperPower = superPower;
+        mOneThing = oneThing;
+        mFileName = mUserName + ".png";
+    }
+
+    /**
+     * Gets the username of the <code>Superhero</code>.
+     * @return The username of the <code>Superhero</code>.
+     */
+    public String getUserName()
+    {
+        return mUserName;
+    }
+
+    /**
+     * Gets the name of the <code>Superhero</code>.
+     * @return The name of the <code>Superhero</code>.
+     */
+    public String getName()
+    {
+        return mName;
+    }
+
+    /**
+     * Gets the superpower of the <code>Superhero</code>.
+     * @return The superpower of the <code>Superhero</code>.
+     */
+    public String getSuperPower()
+    {
+        return mSuperPower;
+    }
+
+    /**
+     * Gets the one thing about the <code>Superhero</code>.
+     * @return The one thing about the <code>Superhero</code>.
+     */
+    public String getOneThing()
+    {
+        return mOneThing;
+    }
+
+    /**
+     * Gets the file name of the <code>Superhero</code> with its path. For example: dtran258.png
+     * @return The file name of the <code>Superhero</code>.
+     */
+    public String getFileName()
+    {
+        return mFileName;
+    }
+
+    /**
+     * Generates a text based representation of this <code>Superhero</code>.
+     * @return A text based representation of this <code>Superhero</code>.
+     */
+    @Override
+    public String toString()
+    {
+        return "Superhero{" + "mUserName='" + mUserName + '\'' + ", mName='" + mName + '\''
+                + ", mSuperPower='" + mSuperPower + '\'' + ", mOneThing='" + mOneThing + '\''
+                + ", mFileName='" + mFileName + '\'' + '}';
+    }
+}
